42b5207feb96bffd58e2df62b3adaaa66af69716
Alex Bellamy <alexjbellamy@gmail.com>
2022-09-17 17:10:06 -0400

    got GET /api/notes working after a little trial and error

--- routes/notes.js
+++ routes/notes.js
@@ -1,14 +1,14 @@
 const notes = require('express').Router();
 const path = require('path');
 const fs = require('fs');
-const dbFile = path.join(__dirname, '/db/db.json');
+const dbFile = './db/db.json';
 
 notes.get('/', (req, res) => {
     fs.readFile(dbFile, 'utf8', (err, data) => {
         if (err) {
             console.error(err);
         } else {
-            res.json(data);
+            return res.json(JSON.parse(data));
         }
     })
 });
