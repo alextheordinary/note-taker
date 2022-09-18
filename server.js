89dcfe1ec9f7e7f1d1f4db7dfbc54956fcfecabc
Alex Bellamy <alexjbellamy@gmail.com>
2022-09-17 20:35:31 -0400

    trying to troubleshoot why app doesn't run on heroku. added express.urlencoded

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.port || 5000;
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 // Used to serve static files.
